cf374f85a095be0b9429f2924216c5a44cce5beb
sullivan <ya.zigmund@yandex.ru>
2014-04-12 21:04:21 +0400

    determSysytemModule

---
+++ WebApplication1/Scripts/determSystem.js
@@ -0,0 +1,85 @@
+﻿var determInitSystem = (function () {
+    var view = {};
+    var initSystem = new InitSystem();
+    var elementsHashList = [];
+
+    function InitSystem(system) {
+        this.name = system && system.name;
+        this.goal = system && system.goal;
+        this.elements = [];
+    }
+
+    InitSystem.prototype = {
+    }
+
+    function SystemElement(element) {
+        this.name = element && element.name;
+        this.type = element && element.type;
+        this.role = element && element.role;
+        this.comment = element && element.comment;
+        this.fuzzy = element && element.fuzzy;
+        this.domain = element && element.domain;
+        this.values = element && element.values;
+    }
+
+    SystemElement.prototype = {
+    }
+
+    function getHash(object) {
+        if (object instanceof SystemElement) {
+            return object.name;
+        }
+    }
+
+    function isExist(hashArr, hash) {
+        return (hashArr[hash] != undefined) ? true : false;
+    }
+
+    function removeFrom(arr, index) {
+        for (var i = index; i < arr.length; i++) {
+            arr[i] = arr[i + 1];
+        }        
+        arr.pop();            
+    }
+
+    return {
+        setName: function (name) {
+            initSystem.name = name;
+        },
+        setGoal: function (goal) {
+            initSystem.goal = goal;
+        },
+        addElement: function (element) {
+            var newSystemElement = new SystemElement(element);
+            if (newSystemElement) {
+                var hash = getHash(newSystemElement);
+                if (!isExist(elementsHashList, hash)) {
+                    elementsHashList[hash] = initSystem.elements.length;
+                    initSystem.elements.push(newSystemElement);
+                } else {
+                }
+            }
+        },
+        removeElement: function (element) {
+            if (element) {
+                var hash = getHash(element);
+                if (isExist(elementsHashList, hash)) {
+                    var index = elementsHashList[hash];
+                    removeFrom(initSystem.elements, index);
+                    delete elementsHashList[hash];
+                } else {
+                }
+            }
+        },
+        getSystem: function (){
+            return initSystem;
+        }
+    }
+}());
+
+
+
+
+
+
+
---
+++ WebApplication1/Scripts/determSystemTest.js
@@ -0,0 +1,39 @@
+﻿test("determSystem", function (){
+    
+    var initSystem = determInitSystem;
+
+    initSystem.setName("<система>");
+    initSystem.setGoal("<цель>");
+    equal(initSystem.getSystem().name, "<система>", 'set name');
+    equal(initSystem.getSystem().goal, "<цель>", 'set goal');
+
+    for (var i = 0; i < 2; i++) {
+        initSystem.addElement({name:"<name>"});
+        console.log(initSystem);
+    }
+    
+    equal(initSystem.getSystem().elements[0].name, "<name>", 'add 2 elements');
+    for (var i = 0; i < initSystem.getSystem().elements.length; i++) {
+        initSystem.removeElement(initSystem.getSystem().elements[i]);
+    }
+
+});
+
+test('remove from array', function () {
+    var arr = [1, 4, 3, 5, 26, 3];
+    removeFrom(arr, 0);
+    equal(arr[0], 4, 'remove first');
+    equal(arr.length, 5, 'new length');
+
+});
+
+test('hash for object', function () {
+    var newElement = new SystemElement({ name: "<name>" });
+    var hash = getHash(newElement);
+    equal(hash, "<name>", 'hash for element');
+
+    var hashArr = [];
+    hashArr[hash] = 0;
+
+    ok(isExist, 'exist element');
+});
